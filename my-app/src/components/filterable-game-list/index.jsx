a987b82c53b5460d786c54009cce0893a37a07f4
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-09-30 23:07:10 +0200

    start1

--- my-app/src/components/filterable-game-list/index.jsx
+++
@@ -1,9 +0,0 @@
-import SearchBar from "../search-bar";
-
-export default function FilterableGameList({ games = [] }) {
-  return (
-    <div className="filterable-game-list">
-      <SearchBar/>
-    </div>
-  )
-}
