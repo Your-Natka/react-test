96b2d8275738e4bd22454be875d085d847616b49
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-10-02 19:58:36 +0200

    gameRow

--- src/components/game-gallery/index.jsx
+++ src/components/game-gallery/index.jsx
@@ -4,7 +4,8 @@ export default function GameGallery({
     images =[]
 }) {
     let gallery = images.map (
-        (image, index) => <img src={image} alt='preview' key={index}/>)
+        (image, index) => <img src={image} alt="preview" key={index}/>
+)
   return (
     <div className='game-gallery'>
       {gallery}
--- src/components/game-row/index.jsx
+++ src/components/game-row/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import GameGallery from '../game-gallery'
 
-export default function GameRow( game = {}) {
+export default function GameRow({ game = {} }) {
   return (
     <li className='game-row'>
       <span className='game-name'>{game.name}</span>
       { game.inWishList && <span className='game-in-whish-list'>В желаемом</span>}
-    <GamaGallery images={game.images}/>
+    <GameGallery images={game.images}/>
     <span className="game-price">{game.price}</span>
     </li>
   )
